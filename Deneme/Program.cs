b505139e14ef98ce147e4039acda9ae41089d8e3
OnurSen <onur.sen@email.com>
2023-03-06 22:48:53 +0300

    first commit

---
+++ Deneme/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deneme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Deneme");
+
+        }
+    }
+}
